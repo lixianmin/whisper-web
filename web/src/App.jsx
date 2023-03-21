345f64ea84b206a654ebed63e650e618a9b6a885
lixianmin <lixianmin@gmail.com>
2023-03-21 22:04:41 +0800

    加载好像可以了

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -7,8 +7,43 @@
 import './App.module.css';
 import AudioRecorder from "./widgets/AudioRecorder";
 import {render} from "solid-js/web";
+import {loadRemote} from "./code/helpers";
 
 function App() {
+
+
+    function storeFS(fname, buf) {
+        // write to WASM file using FS_createDataFile
+        // if the file exists, delete it
+        try {
+            Module.FS_unlink(fname);
+        } catch (e) {
+            // ignore
+        }
+
+        Module.FS_createDataFile("/", fname, buf, true, true);
+
+        printTextarea('storeFS: stored model: ' + fname + ' size: ' + buf.length);
+    }
+
+    function loadWhisper() {
+        let url     = 'https://localhost/ggml-model-whisper-base.bin'
+        let dst     = 'whisper.bin';
+        let size_mb = 142;
+
+        let cbProgress = function(p) {
+            // console.log(p)
+        };
+
+        const cbCancel = function() {
+            console.log('canceled')
+        }
+
+        loadRemote(url, dst, size_mb, cbProgress, storeFS, cbCancel, printTextarea);
+    }
+
+    loadWhisper()
+
     return <>
         <AudioRecorder/>
     </>
--- web/vite.config.js
+++ web/vite.config.js
@@ -3,7 +3,18 @@ import solidPlugin from 'vite-plugin-solid'
 import path from 'path'
 
 export default defineConfig({
-    plugins: [solidPlugin()],
+    plugins: [solidPlugin(),
+        {
+            name: 'configure-response-headers',
+            configureServer: server => {
+                server.middlewares.use((_req, res, next) => {
+                    res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+                    res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+                    next();
+                });
+            }
+        }
+    ],
     server: {
         port: 3000,
         https: {
