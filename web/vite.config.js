7c8a5d592ecc6cf76a37a3f482845333393e155f
lixianmin <lixianmin@gmail.com>
2023-03-21 20:03:39 +0800

    vite启动时支持https了

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,11 +1,15 @@
-import {defineConfig} from 'vite';
-import solidPlugin from 'vite-plugin-solid';
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import {defineConfig} from 'vite'
+import solidPlugin from 'vite-plugin-solid'
+import path from 'path'
 
 export default defineConfig({
-    plugins: [solidPlugin(), basicSsl()],
+    plugins: [solidPlugin()],
     server: {
-        port: 3000
+        port: 3000,
+        https: {
+            key: path.resolve(__dirname, '../res/ssl/localhost.key'),
+            cert: path.resolve(__dirname, '../res/ssl/localhost.crt'),
+        }
     },
     build: {
         target: 'esnext',
